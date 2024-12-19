e9f889adb3cd6ca3d657bbbf3b01ad56f861b9bb
vinoth <pktvino525@gmail.com>
2024-12-19 19:12:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# React JS Startup and small business website template
+
+### This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+# Screenshot
+
+![JMtax edit](https://user-images.githubusercontent.com/43953425/212030864-cf3aa272-e070-4e1b-8e25-f488ca56873e.png)
+
+
+
+
+## Description
+
+This is a ReactJS based startup and small business website template. 
+It was designed using TailwindCSS and React. 
+The Contact form was made functional using EmailJS, to learn how to use EmailJS, you can check out the official documentation
+## How to use it
+
+To use this template:
+1. Clone the repository to your local machine, 
+2. run `npm install` to install all the required dependencies and
+3. run `npm start` to view the website locally.
+Feel free to edit and make changes. 
+
+You can also Fork the repo and clone to your local machine, make desired changes and enjoy.
---
+++ src/components/Portfolio.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Portfolio = () => {
+    return (
+        <>
+            <div className="my-4 py-4" id='portfolio'>
+                <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">Products</h2>
+                <div className='flex justify-center'>
+                    <div className='w-24 border-b-4 border-blue-900 mb-8'></div>
+                </div>
+
+                <div className="px-4" data-aos="fade-down" data-aos-delay="600">
+                    <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-5">                            
+                        <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3 min-h-max">
+                            <div className="m-2 text-justify text-sm">
+                                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">Top Tax Tips for Individuals</h4>
+                                <p className="text-md font-medium leading-5 h-auto md:h-48">
+                                Navigating taxes can feel overwhelming, but a few smart strategies can help you save money and avoid headaches. Start by organizing your financial records—keep track of income, expenses, and receipts for deductions. Maximize contributions to tax-advantaged accounts, such as retirement plans or health savings accounts, to lower your taxable income.
+                                </p>
+                                <div className="flex justify-center my-4">
+                                    <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
+                                        Schedule Demo
+                                        <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="bg-white transition-all ease-in-out duration-400 overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
+                            <div className="m-2 text-justify text-sm">
+                                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">Essentials for Small Businesses</h4>
+                                <p className="text-md font-medium leading-5 h-auto md:h-48">
+                                Running a small business is a rewarding yet challenging journey, especially when it comes to managing finances and taxes.She loved crafting delicious pastries but struggled with understanding tax regulations and managing her finances efficiently For many business owners, the complexity of tax laws and the time-consuming process of staying compliant can feel overwhelming.
+                                </p>
+                                <div className="flex justify-center my-4">
+                                    <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
+                                        Schedule Demo
+                                        <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                    </Link>
+                                </div>
+                                
+                                {/* <ul className="border-t border-gray-300 py-8 space-y-6">
+                                    <li className="flex items-center space-x-2 px-8">
+                                        <span className="bg-blue-600 rounded-full p-1">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 text-white" viewBox="0 0 20 20" fill="currentColor">
+                                                <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
+                                            </svg>
+                                        </span>
+                                        <span className="text-gray-600 capitalize">Free Setup Guidance</span>
+                                    </li>
+                                </ul> */}
+                            </div>
+                        </div>
+
+                        <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
+                            <div className="m-2 text-justify text-sm">
+                            <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">How to Prepare for Tax Season</h4>
+                                <p className="text-md font-medium leading-5 h-auto md:h-48">
+                                Tax season can be stressful, but early preparation can make the process smoother and more manageable. Start by organizing all your financial documents, including income statements, receipts for deductible expenses, and records of investments.Consider using tax software or hiring a professional to ensure accuracy and identify opportunities for savings 
+                                 </p>
+                                <div className="flex justify-center my-4">
+                                    <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
+                                        Schedule Demo
+                                        <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
+                            <div className="m-2 text-justify text-sm">
+                                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">Event Management System</h4>
+                                <p className="text-md font-medium leading-5 h-auto md:h-48">
+                                    Our event management system helps to manage different types of events, whether weddings, burials or any kind of event. With an easily customizable menu, you get to define your user experience and make your event fully customizable and memorable.
+                                    With use cases already recorded for quite a number of events, we can assure you of a stress-less event mangement platform.
+                                </p>
+                                <div className="flex justify-center my-4">
+                                    <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
+                                        Schedule Demo
+                                        <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>                    
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Portfolio;
